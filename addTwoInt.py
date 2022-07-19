1f7194d4637cb848e765d0447e815a336b7ea21c
arnorbert <adriennorbert32@gmail.com>
2022-07-19 11:19:20 +0400

    modification of addTwoInt.py to handle input errors

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,13 +1,16 @@
 #!/bin/env python
+
 import sys
 
 def add(a,b):
 	sum = a + b
 	print(sum)
-
-
-a = int( sys.argv[1] )
-b = int( sys.argv[2] )
+try:
+	a = int( sys.argv[1] )
+	b = int( sys.argv[2] )
+except IndexError:
+	print("Error: You must give 2 numerical arguments.")
+	raise (SystemExit)
 
 add(a,b)
 
